d4b087dd10166d09df6d9fba1b2940bea908d7de
Ivan Raštegorac <ivan.rastegorac@gmail.com>
2022-05-23 13:13:50 +0200

    implemented charts in Ecommerce and Button component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
   }, [screenSize]);
 
   return (
-    <div className="flex justify-between p-2 md:mx-6 relative">
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
       <NavButton
         title="Menu"
         customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
